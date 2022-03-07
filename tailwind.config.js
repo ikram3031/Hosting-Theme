bad74a58d4f7b2b8976e144b0181a82091dcc1fb
ikram3031 <mdikram.3031@gmail.com>
2022-03-07 21:46:28 +0600

    start

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [],
+  theme: {
+    backgroundSize: {
+      'auto': 'auto',
+      'cover': 'cover',
+      'contain': 'contain',
+     '50%': '50%',
+     '16': '4rem',
+    },
+    extend: {
+      backgroundImage:{
+        'hero': "url(/imgs/hero_bg.png)"
+      },
+    },
+  },
+  plugins: [],
+}
